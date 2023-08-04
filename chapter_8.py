bb1cb5781c7ccc8f44fdc82954677412feb33cc0
Finn345 <85902219+Finn345@users.noreply.github.com>
2023-08-04 01:14:38 -0500

    Delete chapter_8.py

--- chapter_8.py
+++
@@ -1,9 +0,0 @@
-def is_prime(num):
-    """Naive method of checking for primes"""
-    for n in range(2,num):
-        if num % n == 0:
-            print('not prime')
-            break
-        else: #If never mod zero, then prime
-            print('prime')
-is_prime(25)
